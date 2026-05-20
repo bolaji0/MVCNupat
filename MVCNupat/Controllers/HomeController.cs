fc639fd307626a518db24a8d148779e5b284df10
bolaji0 <116122775+bolaji0@users.noreply.github.com>
2026-05-20 19:31:46 +0100

    Add Ten action method to HomeController

--- MVCNupat/Controllers/HomeController.cs
+++ MVCNupat/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace MVCNupat.Controllers
             return View();
         }
 
+        public IActionResult Ten()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
